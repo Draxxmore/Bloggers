c65566df2ca2d596197e0913e64b15c288dd6691
William Scarset <wscarset@gmail.com>
2022-06-03 16:26:18 -0500

    CORS again

--- bloggers_backend/src/api/routes/index.js
+++ bloggers_backend/src/api/routes/index.js
@@ -4,6 +4,9 @@ const registrationRoutes = require("./registration");
 const userRoutes = require("./users");
 const loginRoute = require("./login");
 const blogRoute = require("./blog");
+const cors = require("cors");
+
+router.use(cors());
 
 router.use("/registration", registrationRoutes);
 router.use("/users", userRoutes);
