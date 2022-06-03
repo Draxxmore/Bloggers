5dacf2d5ea107b3e618d4c5cc7f5073b1287fe02
William Scarset <wscarset@gmail.com>
2022-06-03 18:18:00 -0500

    More Cors

--- bloggers_backend/src/api/routes/registration.js
+++ bloggers_backend/src/api/routes/registration.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { getUsers, postRegistration } = require("../services");
 const bcrypt = require("bcryptjs");
-const cors = require("cors");
-
-router.use(cors());
 
 router.post("/", async (request, response) => {
   const usernameInDB = await getUsers().then((data) =>
@@ -25,7 +22,7 @@ router.post("/", async (request, response) => {
   };
 
   await postRegistration(user)
-    .then((data) => response.status(201).send({ Success: data }))
+    .then((data) => response.status(201).json({ Success: data }))
     .catch((error) => response.status(500).send(error));
 });
 
